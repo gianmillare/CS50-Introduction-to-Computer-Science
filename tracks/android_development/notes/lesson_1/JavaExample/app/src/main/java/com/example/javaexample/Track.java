9cfc1520bf3dfa8928620a240a92d9579be592d2
gianmillare <gianmillaregsm@gmail.com>
2020-08-03 17:22:25 -0700

    completed android dev lesson 1. adding notes and first app

---
+++ tracks/android_development/notes/lesson_1/JavaExample/app/src/main/java/com/example/javaexample/Track.java
@@ -0,0 +1,19 @@
+package com.example.javaexample;
+
+public class Track {
+    private String name;
+    private String instructor;
+
+    Track(String name, String instructor) {
+        this.name = name;
+        this.instructor = instructor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+}
