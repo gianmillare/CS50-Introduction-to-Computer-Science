d8f6ceae23f8a0a3d19effe0e8237684c0e25daf
gianmillare <gianmillaregsm@gmail.com>
2020-08-24 20:58:21 -0700

    pushing notes to declaring variable inside the flask app

--- tracks/web_development/flask_practice/create_a_flask_server/app.py
+++ tracks/web_development/flask_practice/create_a_flask_server/app.py
@@ -1,10 +1,12 @@
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") #This is the default webpage (homepage)
 def index():
-    return render_template("index.html")
+    random_number = random.randint(1, 100)
+    return render_template("index.html", name="Gian Millare", number=random_number)
 
 @app.route("/goodbye")
 def bye():
